a573087e58052e5c9e1a3959e394b1daf3f7c75e
Ridwanul Basit <basitridwanul@gmail.com>
2025-10-02 22:59:41 +0600

    Navbar Updated

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,60 @@
+// TheatreCurtain.jsx
+import React, { useRef, useEffect } from "react";
+import { gsap } from "gsap";
+
+const TheatreCurtain = ({ children }) => {
+  const leftCurtain = useRef(null);
+  const rightCurtain = useRef(null);
+
+  useEffect(() => {
+    // Animate curtains opening
+    gsap.to(leftCurtain.current, { x: "-100%", duration: 2, ease: "power2.inOut" });
+    gsap.to(rightCurtain.current, { x: "100%", duration: 2, ease: "power2.inOut" });
+  }, []);
+
+  return (
+    <div style={{ position: "relative", width: "100%", height: "100vh", overflow: "hidden" }}>
+      {/* Curtains */}
+      <div
+        ref={leftCurtain}
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "50%",
+          height: "100%",
+          background: "white",
+          zIndex: 10,
+        }}
+      />
+      <div
+        ref={rightCurtain}
+        style={{
+          position: "absolute",
+          top: 0,
+          right: 0,
+          width: "50%",
+          height: "100%",
+          background: "white",
+          zIndex: 10,
+        }}
+      />
+
+      {/* Stage content */}
+      <div style={{ 
+        position: "relative", 
+        zIndex: 1, 
+        width: "100%", 
+        height: "100%", 
+        display: "flex", 
+        justifyContent: "center", 
+        alignItems: "center", 
+        backgroundColor: "#fff"  // White stage
+      }}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default TheatreCurtain;
